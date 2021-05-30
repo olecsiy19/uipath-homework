86548171d0163e2a2ca21b61680fad78892c7177
olevytskyi <oleksii.levytskyi@irco.com>
2021-05-30 14:44:26 +0300

    update README file move api key to `.env`

--- src/api.js
+++ src/api.js
@@ -4,7 +4,7 @@ export default axios.create({
 	baseURL: `https://du.uipath.com/`,
 	headers: {
 	"Content-Type": "application/json",
-		"X-UIPATH-License": "7H5G5RrLGrX3KfaFEtmRwkpCinwNCG9OSnerz31WhgGZo7luWlXUxCMVjUUJOOf1zgxsVohSKLof48iUFwBd2A==",
+		"X-UIPATH-License": process.env.REACT_APP_API_KEY,
 		"X-UIPATH-Version": "1"
 	}
 });
